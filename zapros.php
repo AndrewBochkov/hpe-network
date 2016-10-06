c40b638c05d344ef61c976e69bf4cac76e038ae3
YourFullNameAndrewBochkov <andrewbochkov@gmail.com>
2016-10-06 12:00:04 +0300

    first commit

---
+++ zapros.php
@@ -0,0 +1,31 @@
+<html>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<body>
+
+<?php
+require ("library.php.inc"); //.. Библиотека с функциями 
+
+$choices=$_POST['sz'];
+$dates = date("d.m.Y H:i:s");
+$files=	$_SERVER["HTTP_REFERER"];
+$ip = $_SERVER["REMOTE_ADDR"];
+$referer=$_SERVER["HTTP_REFERER"];
+
+list ( $Description, $Products ) = getSession( );
+if ( $Products[0] == 0 ) {
+	print "Need to return to previusly page<br>"; 
+	return;
+} 
+
+//.. TEST::print "before searchFor()<br>\n"; print_r ( $choices ); print "before2 searchFor()<br>\n";
+$mSearch = searchFor( $choices, $Products ); //... Этот Массив ...но он же может быть и пустым
+//.. TEST:: print "after searchFor()<br>\n"; print_r ( $mSearch);
+if ( $mSearch[0] == 0 ) {
+	print "No records find<br>"; 
+} else searchOut( $Description, $mSearch, $choices )
+
+?>
+
+
+</BODY>
+</HTML>
